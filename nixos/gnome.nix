9ccd0f57ac6e01c742d883cb4c1db5cf37e14286
Jason Crevier <jason@jasoncrevier.com>
2024-01-09 22:44:36 -0500

    configured gnome in home-manager

--- nixos/gnome.nix
+++ nixos/gnome.nix
@@ -6,7 +6,6 @@
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
   environment.systemPackages = with pkgs; [
-    gnomeExtensions.tray-icons-reloaded
     remmina
   ];
   environment.gnome.excludePackages = (with pkgs; [
