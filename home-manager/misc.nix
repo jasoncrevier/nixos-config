89674b15477f616eb5d13ad12cdc4905739c6fc4
Jason Crevier <jason@jasoncrevier.com>
2024-01-14 16:24:57 -0500

    home file cleanup

--- home-manager/misc.nix
+++ home-manager/misc.nix
@@ -6,4 +6,11 @@
     (catppuccin-papirus-folders.override { flavor = "mocha"; accent = "teal"; })
     (catppuccin-gtk.override { accents = [ "teal" ]; variant = "mocha"; })
   ];
+
+  # Fix for "command-not-found being" found. Beyond my understanding but it fixed
+  # an issue I was having so I'll work on understanding it later.
+  programs.nix-index = {
+    enable=true;
+    enableBashIntegration = true;
+  };
 }
