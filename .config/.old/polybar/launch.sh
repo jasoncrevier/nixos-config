af200c846bd0e31156ee21a2975c238ad574447f
Jason Crevier <jason@jasoncrevier.com>
2024-01-14 16:10:23 -0500

    cleaned up old files

--- .config/.old/polybar/launch.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# Terminate already running bar instances
-killall -q polybar
-
-# Launch Polybar, using default config location ~/.config/polybar/config.ini
-polybar example 2>&1 | tee -a /tmp/polybar.log & disown
