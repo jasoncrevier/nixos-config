1fd26de06752f1543c2d2cc403ee3b72d5c6238a
Jason Crevier <jason@jasoncrevier.com>
2024-01-22 21:08:25 -0500

    removed text scaling from gnome

--- home-manager/gnome-home.nix
+++ home-manager/gnome-home.nix
@@ -21,7 +21,6 @@ dconf.settings = {
     };
     "org/gnome/desktop/interface" = {
       enable-hot-corners = false;
-      text-scaling-factor = 1.25;
       clock-format = "12h";
     };
     "org/gtk/settings/file-chooser" = {
