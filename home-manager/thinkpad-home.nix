7102ca67a6f4b5743c405e83ec2c085f96ba5d53
Jason Crevier <jason@jasoncrevier.com>
2024-10-20 14:26:01 -0400

    refactor/rearrange

--- home-manager/thinkpad-home.nix
+++ home-manager/thinkpad-home.nix
@@ -11,21 +11,13 @@
       ./gnome-home.nix
       ./graphics.nix
       ./misc.nix
+      ./syncthing.nix
       ./theme.nix
       ./vscode.nix
       ./web.nix
     ];
   home.username = "jason";
   home.homeDirectory = "/home/jason";
-
-  # Let Home Manager install and manage itself.
-  programs.home-manager.enable = true;
-
-  # Enable Syncthing on this machine
-  services.syncthing.enable = true;
-  home.packages = with pkgs; [
-    syncthing-tray
-  ];
 
   # Don't change this (I think)
   home.stateVersion = "24.05";
