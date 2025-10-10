704bc7e25817223237a8acfd150f1150681ab1ca
Jason Crevier <jason@jasoncrevier.com>
2025-10-10 16:11:00 -0400

    added tower to syncthing

--- nixos/device-configs/office-syncthing.nix
+++ nixos/device-configs/office-syncthing.nix
@@ -5,15 +5,16 @@
     settings = {
       devices = {
         "thinkpad" = { id = "ZYUMO4O-YCWCXF7-LIH7DDZ-7XDHXVW-A6P4FZD-XQVER3U-QIGAVCO-7V36XQL"; };
+        "tower" = { id = "QMSNAMC-4WPXC3P-SGVM42Z-IT3E3TD-DTMXTIN-QCX42SW-PI4IEWB-TDWXYQU"; };
       };
       folders = {
         "home_dot_BitwigStudio" = {
           path = "/home/jason/.BitwigStudio";
-          devices = [ "thinkpad" ];
+          devices = [ "thinkpad" "tower" ];
         };
         "home_Bitwig_Studio" = {
           path = "/home/jason/Bitwig Studio";
-          devices = [ "thinkpad" ];
+          devices = [ "thinkpad" "tower" ];
         };
       };
     };
--- nixos/device-configs/thinkpad-syncthing.nix
+++ nixos/device-configs/thinkpad-syncthing.nix
@@ -5,15 +5,16 @@
     settings = {
       devices = {
         "office" = { id = "UQ5JITA-OXCRVTH-ZASHRVT-7L7MOQX-UO6VYI2-CDTIQLD-VYKUOMI-QMMPDQP"; };
+        "tower" = { id = "QMSNAMC-4WPXC3P-SGVM42Z-IT3E3TD-DTMXTIN-QCX42SW-PI4IEWB-TDWXYQU"; };
       };
       folders = {
         "home_dot_BitwigStudio" = {
           path = "/home/jason/.BitwigStudio";
-          devices = [ "office" ];
+          devices = [ "office" "tower" ];
         };
         "home_Bitwig_Studio" = {
           path = "/home/jason/Bitwig Studio";
-          devices = [ "office" ];
+          devices = [ "office" "tower" ];
         };
       };
     };
