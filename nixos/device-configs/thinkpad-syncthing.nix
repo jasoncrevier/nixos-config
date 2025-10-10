0f6809063eacc6b3257ba4644cf00f974f697de9
Jason Crevier <jason@jasoncrevier.com>
2025-10-10 16:20:55 -0400

    syncthing for thinkpad

--- nixos/device-configs/thinkpad-syncthing.nix
+++ nixos/device-configs/thinkpad-syncthing.nix
@@ -4,7 +4,7 @@
   services.syncthing = {
     settings = {
       devices = {
-        "office" = { id = "UQ5JITA-OXCRVTH-ZASHRVT-7L7MOQX-UO6VYI2-CDTIQLD-VYKUOMI-QMMPDQP"; };
+        "office" = { id = "3BUNMEZ-TWPP4MZ-Z2G7BAO-ZY5ZLSX-ENN5YON-AINIIGZ-CG4LHSI-VUESUQ2"; };
       };
       folders = {
         "home_dot_BitwigStudio" = {
