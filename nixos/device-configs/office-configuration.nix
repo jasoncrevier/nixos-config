0d6c1b579cb0e2a0834457d18084e64b9136c7cb
Jason Crevier <jason@jasoncrevier.com>
2025-10-12 10:55:16 -0400

    added script to correct sddm monitor layout

--- nixos/device-configs/office-configuration.nix
+++ nixos/device-configs/office-configuration.nix
@@ -28,4 +28,27 @@
 
   #~Hostname~
   networking.hostName = "office";
+
+  system.activationScripts.sddmCopyDisplayConfig = {
+  # We use a high priority to make sure this runs late in the activation process.
+  # The name must be unique, like 'sddmCopyDisplayConfig'.
+  text = ''
+    # Define the source and destination paths
+    SOURCE_FILE="/home/jason/.config/kwinoutputconfig.json"
+    DEST_DIR="/var/lib/sddm/.config"
+    DEST_FILE="$DEST_DIR/kwinoutputconfig.json"
+
+    # Ensure the destination directory exists
+    if [ ! -d "$DEST_DIR" ]; then
+        mkdir -p "$DEST_DIR"
+        chown sddm:sddm "$DEST_DIR"
+    fi
+
+    # Copy the file only if it exists
+    if [ -f "$SOURCE_FILE" ]; then
+        cp "$SOURCE_FILE" "$DEST_FILE"
+        chown sddm:sddm "$DEST_FILE"
+    fi
+    '';
+  };
 }
