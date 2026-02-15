6533f7d6326f4ebe48b0e7ae37354ba4787fdc0d
Jason Crevier <jason@jasoncrevier.com>
2026-02-15 13:39:13 -0500

    Updated update alias to nix run home-manager

--- home-manager/fish.nix
+++ home-manager/fish.nix
@@ -7,7 +7,7 @@
     set fish_greeting # Disable greeting
     '';
     shellAliases = {
-      update = "cd ~/nixos-config && sudo nix flake update && sudo nixos-rebuild switch --flake './' && home-manager switch --flake './'";
+      update = "cd ~/nixos-config && sudo nix flake update && sudo nixos-rebuild switch --flake './' && nix run home-manager switch --flake './'";
       home-update = "cd ~/nixos-config && home-manager switch --flake './'";
     };
   };
