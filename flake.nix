7f3fec30d4211c88ac436d05b7691be1999e9eb0
Jason Crevier <jason@jasoncrevier.com>
2026-02-22 12:46:58 -0500

    created overlay for yabridge PR, reorganized flake to have home-maanger follow nixpkgs

--- flake.nix
+++ flake.nix
@@ -1,8 +1,11 @@
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    # Pull the master branch specifically for the yabridge fix
+    nixpkgs-master.url = "github:nixos/nixpkgs/master";
+
     home-manager = {
-      url = github:nix-community/home-manager;
+      url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     musnix.url = "github:musnix/musnix";
@@ -10,69 +13,87 @@
     vscode-server.url = "github:nix-community/nixos-vscode-server";
   };
 
-  outputs = { nixpkgs, home-manager, catppuccin, musnix, vscode-server, ... }@inputs:
+  outputs = { nixpkgs, nixpkgs-master, home-manager, catppuccin, musnix, vscode-server, ... }@inputs:
+    let
+      system = "x86_64-linux";
 
-  let
-    system = "x86_64-linux";
-    pkgs = import nixpkgs {
-      inherit system;
-      config.allowUnfree = true;
-    };
-  in
-  {
-    nixosConfigurations = {
-      thinkpad = nixpkgs.lib.nixosSystem {
+      # Define the overlay to swap yabridge
+      yabridge-overlay = final: prev: {
+        yabridge = (import nixpkgs-master {
+          inherit system;
+          config.allowUnfree = true;
+        }).yabridge;
+      };
+
+      nixpkgs-config = {
         inherit system;
-        inherit pkgs;
-        modules = [
-          musnix.nixosModules.musnix
-          catppuccin.nixosModules.catppuccin
-          ./nixos/device-configs/thinkpad-configuration.nix
-        ];
+        config.allowUnfree = true;
+        overlays = [ yabridge-overlay ];
       };
 
-      office = nixpkgs.lib.nixosSystem {
-        inherit system;
-        inherit pkgs;
-        modules = [
-          musnix.nixosModules.musnix
-          catppuccin.nixosModules.catppuccin
-          ./nixos/device-configs/office-configuration.nix
-        ];
+      pkgs-hm = import nixpkgs nixpkgs-config;
+    in
+    {
+      nixosConfigurations = {
+        thinkpad = nixpkgs.lib.nixosSystem {
+          inherit system;
+          modules = [
+            # Apply nixpkgs config and overlay via a module to avoid scope issues
+            {
+              nixpkgs.overlays = [ yabridge-overlay ];
+              nixpkgs.config.allowUnfree = true;
+            }
+            musnix.nixosModules.musnix
+            catppuccin.nixosModules.catppuccin
+            ./nixos/device-configs/thinkpad-configuration.nix
+          ];
+        };
+
+        office = nixpkgs.lib.nixosSystem {
+          inherit system;
+          modules = [
+            {
+              nixpkgs.overlays = [ yabridge-overlay ];
+              nixpkgs.config.allowUnfree = true;
+            }
+            musnix.nixosModules.musnix
+            catppuccin.nixosModules.catppuccin
+            ./nixos/device-configs/office-configuration.nix
+          ];
+        };
+
+        nix-server = nixpkgs.lib.nixosSystem {
+          inherit system;
+          modules = [
+            { nixpkgs.config.allowUnfree = true; }
+            ./nixos/device-configs/nix-server-configuration.nix
+          ];
+        };
       };
 
-      nix-server = nixpkgs.lib.nixosSystem {
-        inherit system;
-        inherit pkgs;
-        modules = [
-          ./nixos/device-configs/nix-server-configuration.nix
-        ];
+      homeConfigurations = {
+        "jason@thinkpad" = home-manager.lib.homeManagerConfiguration {
+          pkgs = pkgs-hm;
+          modules = [
+            catppuccin.homeModules.catppuccin
+            ./home-manager/device-configs/thinkpad-home.nix
+          ];
+        };
+
+        "jason@office" = home-manager.lib.homeManagerConfiguration {
+          pkgs = pkgs-hm;
+          modules = [
+            catppuccin.homeModules.catppuccin
+            ./home-manager/device-configs/office-home.nix
+          ];
+        };
+
+        "jason@nix-server" = home-manager.lib.homeManagerConfiguration {
+          pkgs = pkgs-hm;
+          modules = [
+            ./home-manager/device-configs/nix-server-home.nix
+          ];
+        };
       };
     };
-
-    homeConfigurations = {
-      "jason@thinkpad" = home-manager.lib.homeManagerConfiguration {
-        inherit pkgs;
-        modules = [
-          catppuccin.homeModules.catppuccin
-          ./home-manager/device-configs/thinkpad-home.nix
-        ];
-      };
-
-      "jason@office" = home-manager.lib.homeManagerConfiguration {
-        inherit pkgs;
-        modules = [
-          catppuccin.homeModules.catppuccin
-          ./home-manager/device-configs/office-home.nix
-        ];
-      };
-
-      "jason@nix-server" = home-manager.lib.homeManagerConfiguration {
-        inherit pkgs;
-        modules = [
-          ./home-manager/device-configs/nix-server-home.nix
-        ];
-      };
-    };
-  };
 }
