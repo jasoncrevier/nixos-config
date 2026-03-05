188d8fcb7da888d7495510e20593a2fd51bdc8d7
Jason Crevier <jason@jasoncrevier.com>
2026-03-05 16:38:24 -0500

    updated thought record script

--- scripts/thought-record-notification.nix
+++ scripts/thought-record-notification.nix
@@ -12,6 +12,7 @@ let
       TOKEN=$(cat "${ntfyTokenFile}")
       
       curl \
+        -H "Tags: warning, computer" \
         -H "Authorization: Bearer $TOKEN" \
         -H "Title: Any thoughts to record?" \
         -H "Click: ${obsidianUri}" \
