3b17e74eef2a45a5c2f09ee52ee8f774aec6678a
Jason Crevier <jason@jasoncrevier.com>
2026-07-26 16:33:04 -0400

    more audio tweaks

--- nixos/audio.nix
+++ nixos/audio.nix
@@ -11,8 +11,30 @@
     alsa.enable = true;
     pulse.enable = true;
     jack.enable = true;
+    extraConfig.pipewire = {
+      "10-clock-rate" = {
+        "context.properties" = {
+          "default.clock.rate" = 44100;
+          "default.clock.allowed-rates" = [ 44100 ];
+          "default.clock.quantum" = 256;
+          "default.clock.min-quantum" = 256;
+          "default.clock.max-quantum" = 256;
+        };
+      };
+    };
   };
   
+  services.pipewire.wireplumber.extraConfig = {
+    "99-disable-suspend" = {
+      "monitor.alsa.rules" = [
+        {
+          matches = [ { "node.name" = "~alsa_input.*"; } { "node.name" = "~alsa_output.*"; } ];
+          actions = { update-props = { "session.suspend-timeout-seconds" = 0; }; };
+        }
+      ];
+    };
+  };
+
   security.pam.loginLimits = [
     { domain = "@audio"; item = "rtprio"; type = "-"; value = "95"; }
     { domain = "@audio"; item = "memlock"; type = "-"; value = "unlimited"; }
