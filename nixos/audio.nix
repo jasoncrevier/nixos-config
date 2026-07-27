cd0ebf9111e4cb537ea921f7a0ecfd9112e7aae5
Jason Crevier <jason@jasoncrevier.com>
2026-07-27 15:54:26 -0400

    audio fixes

--- nixos/audio.nix
+++ nixos/audio.nix
@@ -1,42 +1,46 @@
-# Enable audio
-
 { pkgs, config, ... }:
 
 {
-
-  security.rtkit.enable = true;
+  boot.kernelParams = [ 
+    "processor.max_cstate=1" 
+    "intel_idle.max_cstate=1" 
+    "usbcore.autosuspend=-1" 
+  ];
 
   services.pipewire = {
     enable = true;
     alsa.enable = true;
     pulse.enable = true;
     jack.enable = true;
+
     extraConfig.pipewire = {
       "10-clock-rate" = {
         "context.properties" = {
-          "default.clock.rate" = 44100;
-          "default.clock.allowed-rates" = [ 44100 ];
-          "default.clock.quantum" = 256;
-          "default.clock.min-quantum" = 256;
-          "default.clock.max-quantum" = 256;
+          "default.clock.rate" = 48000;
+          "default.clock.allowed-rates" = [ 44100 48000 ];
+          "default.clock.min-quantum" = 128;
+          "default.clock.max-quantum" = 1024;
+        };
+      };
+      "99-disable-bluez-midi" = {
+        "bluez5.msbc-support" = false;
+        "bluez5.midi.service" = false;
+      };
+    };
+
+    wireplumber.extraConfig = {
+      "99-disable-v4l2" = {
+        "monitor.v4l2.properties" = {
+          "v4l2.enabled" = false;
+        };
+      };
+      "99-disable-bluez-midi-wp" = {
+        "wireplumber.profiles" = {
+          "main" = {
+            "monitor.bluez-midi" = "disabled";
+          };
         };
       };
     };
   };
-  
-  services.pipewire.wireplumber.extraConfig = {
-    "99-disable-suspend" = {
-      "monitor.alsa.rules" = [
-        {
-          matches = [ { "node.name" = "~alsa_input.*"; } { "node.name" = "~alsa_output.*"; } ];
-          actions = { update-props = { "session.suspend-timeout-seconds" = 0; }; };
-        }
-      ];
-    };
-  };
-
-  security.pam.loginLimits = [
-    { domain = "@audio"; item = "rtprio"; type = "-"; value = "95"; }
-    { domain = "@audio"; item = "memlock"; type = "-"; value = "unlimited"; }
-  ];
 }
